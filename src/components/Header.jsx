1d873c833b8a2c9693981793262cebf151f6353e
Maximilian Neßlauer <maximiliannesslauer@gmail.com>
2024-04-15 13:23:00 +0200

    added smooth scrolling to navlinks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,33 +7,39 @@ export default function HeaderComponent() {
 
   useEffect(() => {
     const handleScroll = () => {
-        const headerVisible = headerRef.current && headerRef.current.getBoundingClientRect().top >= 0;
-        setIsActive(!headerVisible);
+      const headerVisible = headerRef.current && headerRef.current.getBoundingClientRect().top >= 0;
+      setIsActive(!headerVisible);
     };
 
     window.addEventListener('scroll', handleScroll);
     return () => {
-        window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
-}, []);
+  }, []);
 
+  const handleNavLinkClick = (event, targetId) => {
+    event.preventDefault();
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
-    <header ref={headerRef}>
-        <div className={isActive ? "header-container active" : "header-container"}>
-            <div className="header-title">
-                <h1>Maximilian Neßlauer</h1>
-                <p>Computer Science Student | Technology Enthusiast</p>
-            </div>
-            <nav className={`navbar ${isActive ? "centered" : ""}`} aria-label="Main navigation">
-              <a href="/" className="nav-link" aria-current="page">Home</a>
-              <a href="/projects" className="nav-link">Projects</a>
-              <a href="/skills" className="nav-link">Skills</a>
-              <a href="/education" className="nav-link">Education</a>
-              <a href="/contact" className="nav-link">Contact</a>
-            </nav>
+    <header id="home" ref={headerRef}>
+      <div className={isActive ? "header-container active" : "header-container"}>
+        <div className="header-title">
+          <h1>Maximilian Neßlauer</h1>
+          <p>Computer Science Student | Technology Enthusiast</p>
         </div>
+        <nav className={`navbar ${isActive ? "centered" : ""}`} aria-label="Main navigation">
+          <a href="#home" className="nav-link" aria-current="page" onClick={(e) => handleNavLinkClick(e, 'home')}>Home</a>
+          <a href="#projects" className="nav-link" onClick={(e) => handleNavLinkClick(e, 'projects')}>Projects</a>
+          <a href="#skills" className="nav-link" onClick={(e) => handleNavLinkClick(e, 'skills')}>Skills</a>
+          <a href="#education" className="nav-link" onClick={(e) => handleNavLinkClick(e, 'education')}>Education</a>
+          <a href="#contact" className="nav-link" onClick={(e) => handleNavLinkClick(e, 'contact')}>Contact</a>
+        </nav>
+      </div>
     </header>
   );
-
 }
